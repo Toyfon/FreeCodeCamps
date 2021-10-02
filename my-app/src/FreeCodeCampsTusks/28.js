255508c41592dd2b846dd091739ec5fd918eccc2
Toyfon <90236783+Toyfon@users.noreply.github.com>
2021-10-02 13:56:06 +0300

    Tusk 28 (Set someAdjective to a string of at least 3 characters and append it to myStr using the += operator.)

---
+++ my-app/src/FreeCodeCampsTusks/28.js
@@ -0,0 +1,7 @@
+
+//Set someAdjective to a string of at least 3 characters and append it to myStr using the += operator.
+
+
+var someAdjective = "Funny!";
+var myStr = "Learning to code is ";
+myStr += someAdjective

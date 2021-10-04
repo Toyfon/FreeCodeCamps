4e3b4eaa4a24a7154734e48549b8593f4af9ce81
Toyfon <90236783+Toyfon@users.noreply.github.com>
2021-10-04 11:10:37 +0300

    Tusk 35 ( Use bracket notation to find the second-to-last character in the lastName string.
    Hint: Try looking at the example above if you get stuck.)

---
+++ my-app/src/FreeCodeCampsTusks/Basic JS/35.js
@@ -0,0 +1,13 @@
+
+
+//Use bracket notation to find the second-to-last character in the lastName string.
+//
+// Hint: Try looking at the example above if you get stuck.
+
+
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line
+var secondToLastLetterOfLastName = lastName[lastName.length -2]; // Change this line

de19d402285d3696d182ddda9097afc0807258af
Toyfon <90236783+Toyfon@users.noreply.github.com>
2021-10-04 08:51:07 +0300

    Tusk 33 ( Use bracket notation to find the last character in the lastName variable. Hint: Try looking at the example above if you get stuck.)

---
+++ my-app/src/FreeCodeCampsTusks/Basic JS/34.js
@@ -0,0 +1,15 @@
+
+
+
+//Use bracket notation to find the last character in the lastName variable.
+// Hint: Try looking at the example above if you get stuck.
+
+
+
+
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line
+var lastLetterOfLastName = lastName[lastName.length - 1]; // Change this line

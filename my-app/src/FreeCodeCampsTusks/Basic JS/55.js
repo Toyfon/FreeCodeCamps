0c12c39086cfcadae09894b6331bfd3c22abb405
Toyfon <90236783+Toyfon@users.noreply.github.com>
2021-10-05 12:08:01 +0300

    55 (The nextInLine function should then return the element that was removed.)

---
+++ my-app/src/FreeCodeCampsTusks/Basic JS/55.js
@@ -0,0 +1,23 @@
+
+//The nextInLine function should then return the element that was removed.
+
+
+
+
+function nextInLine(arr, item) {
+    // Your code here
+
+    var queue = arr.push(item);
+
+    var removeItem = arr.shift();
+
+    return removeItem;  // Change this line
+}
+
+// Test Setup
+var testArr = [1,2,3,4,5];
+
+// Display Code
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6)); // Modify this line to test
+console.log("After: " + JSON.stringify(testArr));

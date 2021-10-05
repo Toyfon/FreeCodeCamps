f1d33cd18455baf4c81f8e7cf7ab859afe1205dc
Toyfon <90236783+Toyfon@users.noreply.github.com>
2021-10-05 08:53:41 +0300

    52 (Create a function timesFive that accepts one argument, multiplies it by 5, and returns the new value.)

---
+++ my-app/src/FreeCodeCampsTusks/Basic JS/52.js
@@ -0,0 +1,17 @@
+
+
+//Create a function timesFive that accepts one argument, multiplies it by 5, and returns the new value.
+
+
+
+// Example
+function minusSeven(num) {
+    return num - 7;
+}
+
+// Only change code below this line
+function timesFive(num) {
+    return num * 5;
+}
+
+var answer = timesFive(5);

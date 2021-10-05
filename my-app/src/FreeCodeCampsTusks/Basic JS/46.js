257be9f843146736295a202712e1694197135e1c
Toyfon <90236783+Toyfon@users.noreply.github.com>
2021-10-05 08:06:53 +0300

    46 (//The first element in each sub-array should contain a string with the name of the item. The second element should be a number representing the quantity i.e.     //     // ["Chocolate Bar", 15]     // There should be at least 5 sub-arrays in the list. )

---
+++ my-app/src/FreeCodeCampsTusks/Basic JS/46.js
@@ -0,0 +1,13 @@
+//The first element in each sub-array should contain a string with the name of the item. The second element should be a number representing the quantity i.e.
+//
+// ["Chocolate Bar", 15]
+// There should be at least 5 sub-arrays in the list.
+
+
+
+
+var myList = [["vova",30],["Leha",32],["Tanya", 22],["Maks",28],["Ira",25]];
+
+
+
+

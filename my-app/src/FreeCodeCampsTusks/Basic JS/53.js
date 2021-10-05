8c48f649a8ceee53306f3d2b5c6b0d3fa4705105
Toyfon <90236783+Toyfon@users.noreply.github.com>
2021-10-05 12:02:14 +0300

    53 (Create a function addFive without any arguments. This function adds 5 to the sum variable, but its returned value is undefined.)

---
+++ my-app/src/FreeCodeCampsTusks/Basic JS/53.js
@@ -0,0 +1,21 @@
+
+
+//Create a function addFive without any arguments. This function adds 5 to the sum variable, but its returned
+// value is undefined.
+
+
+
+
+var sum = 0;
+
+function addThree() {
+    sum = sum + 3;
+}
+
+function addFive() {
+    sum = sum + 5;
+}
+
+addThree();
+addFive();
+

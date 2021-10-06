ce5f196ed2fcfac7d392f0678cb11d01c1025575
Toyfon <90236783+Toyfon@users.noreply.github.com>
2021-10-06 09:09:38 +0300

    85 (Update the myDog object's name property. Let's change her name from Coder to Happy Coder. You can use either dot or bracket notation.)

---
+++ my-app/src/FreeCodeCampsTusks/Basic JS/85.js
@@ -0,0 +1,14 @@
+ //Update the myDog object's name property. Let's change her name from Coder to Happy Coder. You can use either dot or bracket notation.
+
+
+
+
+ // Setup
+ var myDog = {
+     "name": "Coder",
+     "legs": 4,
+     "tails": 1,
+     "friends": ["freeCodeCamp Campers"]
+ };
+ myDog.name = "Happy Coder"
+ // Only change code below this line

33766c43d2ba53e0715364fb9b04859c18cfb7cd
Toyfon <90236783+Toyfon@users.noreply.github.com>
2021-10-10 18:12:22 +0300

    92 (Using dot and bracket notation, set the variable secondTree to the second item in the trees list from the myPlants object.)

---
+++ my-app/src/FreeCodeCampsTusks/Basic JS/92.js
@@ -0,0 +1,25 @@
+//Using dot and bracket notation, set the variable secondTree to the second item in the trees list from the myPlants
+// object.
+
+
+
+let myPlants = [
+    {
+        type: "flowers",
+        list: [
+            "rose",
+            "tulip",
+            "dandelion"
+        ]
+    },
+    {
+        type: "trees",
+        list: [
+            "fir",
+            "pine",
+            "birch"
+        ]
+    }
+];
+
+let secondTree = myPlants[1].list[1];

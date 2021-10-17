cf3906ec27338b354f675c3c8931404178c78ab4
Toyfon <90236783+Toyfon@users.noreply.github.com>
2021-10-17 15:18:17 +0300

    94 (Add the numbers 5 through 0 (inclusive) in descending order to myArray using a while loop.)

---
+++ my-app/src/FreeCodeCampsTusks/Basic JS/94.js
@@ -0,0 +1,12 @@
+//Add the numbers 5 through 0 (inclusive) in descending order to myArray using a while loop.
+
+// Setup
+var myArray = [];
+var i = 0;
+while (i <= 5){
+    myArray.push(i);
+    i++
+}
+myArray.reverse()
+
+// Only change code below this line
